de6c2437ba92b74b7c92dafcf624efc85e392343
Ahmed Amr <ahmed.amr@businessboomers.net>
2019-10-05 18:58:04 +0200

    supercar edit component

---
+++ src/app/collection/supercar-edit/supercar-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supercar-edit',
+  templateUrl: './supercar-edit.component.html',
+  styleUrls: ['./supercar-edit.component.scss']
+})
+export class SupercarEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
