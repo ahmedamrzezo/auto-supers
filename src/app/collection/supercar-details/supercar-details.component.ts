ec3e600eafae087d2a62f6505fe3ff3853bdc8a2
Ahmed Amr <ahmed.amr@businessboomers.net>
2019-11-11 18:22:16 +0200

    super detail banner content

--- src/app/collection/supercar-details/supercar-details.component.ts
+++ src/app/collection/supercar-details/supercar-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
+
+import { PagesService } from 'src/app/shared/pages.service';
+import { SuperCarService } from '../super-car.service';
+import { SuperCar } from '../super-car';
 
 @Component({
   selector: 'app-supercar-details',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupercarDetailsComponent implements OnInit {
 
-  constructor() { }
+  activeSuper: SuperCar;
+
+  constructor(
+    private _pagesService: PagesService, 
+    private _superCarService: SuperCarService,
+    private router: ActivatedRoute
+    ) {}
 
   ngOnInit() {
+    this.router.params.pipe(take(1)).subscribe((param) => {
+      this.activeSuper = this._superCarService.getSuperByCode(param.code);
+      this._pagesService.bannerContent.next({title: this.activeSuper.carName});
+    })
   }
 
 }
