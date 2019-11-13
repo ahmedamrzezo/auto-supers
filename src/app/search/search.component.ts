e05bc14e7c8468fec17dab36bf574405be588945
AhmedAmr-bbn <Ahmed.Amr@businessboomers.net>
2019-11-13 23:22:39 +0200

    getting started with search logic

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { SuperCarService } from '../collection/super-car.service';
+import { SuperCar } from '../collection/super-car';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -8,11 +11,12 @@ import { NgForm } from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
 
-  searchKeyword: string;
   isSearching: boolean;
   showResults = false;
 
-  constructor() { }
+  searchedSupers: SuperCar[] = []; 
+
+  constructor(private _superCarService: SuperCarService) { }
 
   ngOnInit() {
   }
@@ -21,10 +25,26 @@ export class SearchComponent implements OnInit {
     console.log(form);
     this.isSearching = true;
     this.showResults = true;
-    setTimeout( () => this.isSearching = false, 2000 );
-  }
-  quickSearch() {
-    console.log(this.searchKeyword);
+    this.searchedSupers.splice(0 ,this.searchedSupers.length);
+
+    const keyword = form.value.search;
+    
+    this._superCarService.getSuperCars()
+    .subscribe(supers => {
+      supers.find(sup => {
+        if ( 
+          keyword === sup.carCode || 
+          keyword === sup.carName.toLocaleLowerCase() ||
+          keyword === sup.brandName.toLocaleLowerCase() ||
+          keyword === sup.engineDetails.engineType
+        ) {
+          this.searchedSupers.push(sup);
+          return sup;
+        }
+      })
+      this.isSearching = false;
+      return this.searchedSupers;
+    });
   }
 
 }
