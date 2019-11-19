0a553f3b9baa029fc28f583641a1dda936cfc850
AhmedAmr-bbn <Ahmed.Amr@businessboomers.net>
2019-11-19 23:08:11 +0200

    optimize search and error handling

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SuperCarService } from '../collection/super-car.service';
 import { SuperCar } from '../collection/super-car';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -14,6 +14,7 @@ export class SearchComponent implements OnInit {
   isSearching: boolean;
   showResults = false;
   searchKeyword: string;
+  searchError: string;
 
   searchedSupers: SuperCar[] = []; 
 
@@ -22,30 +23,50 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
   }
   
-  submitSearch(form: NgForm) {
-    console.log(form);
+  submitSearch() {
     this.isSearching = true;
     this.showResults = true;
     
     // emptying the searched arrays before every search
     this.searchedSupers.splice(0 ,this.searchedSupers.length);
     
+    // search for supercar
     this._superCarService.getSuperCars()
+    .pipe(
+      take(1)
+    )
     .subscribe(supers => {
-      supers.filter((sup) => {
+       this.searchedSupers = supers.filter((sup) => {
+        //  string to string matching
         if ( 
-          this.searchKeyword === sup.carCode || 
           this.searchKeyword === sup.carName ||
           this.searchKeyword === sup.brandName ||
           this.searchKeyword === sup.engineDetails.engineType
         ) {
-          this.searchedSupers.push(sup);
           return sup;
         }
+
+        // Regex Approach
+
+        // if ( 
+        //   sup.carName.match(this.searchKeyword) !== null ||
+        //   sup.brandName.match(this.searchKeyword) !== null ||
+        //   sup.engineDetails.engineType.match(this.searchKeyword) !== null
+        // ) {
+        //   return sup;
+        // };
       })
+
+      // stop loading when done
       this.isSearching = false;
       return this.searchedSupers;
-    });
+    },
+    err => {
+      this.isSearching = false;
+      this.searchError = `An Error Occurred, ${err.name}`;
+      throw new Error(`An Error Occurred, ${err.name}`);
+    }
+    );
   }
 
 }
