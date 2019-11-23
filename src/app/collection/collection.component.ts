3807df0b34dcef8737b7e3cdc35fa9357c50b8e2
Ahmed Amr <ahmed.amr@businessboomers.net>
2019-11-23 23:47:29 +0200

    remove todo

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -33,11 +33,3 @@ export class CollectionComponent implements OnInit {
   }
 
 }
-
-/**
- * TODO:
- * - create super card ui [image/title/tags/short-description]
- * - create super interface
- * - define a supers dummy array
- * - display array in collection page
- */
