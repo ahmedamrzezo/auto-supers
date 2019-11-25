388ea65075cde9421ccc998b3c25834bcb952418
Ahmed Amr <ahmed.amr@businessboomers.net>
2019-11-25 21:22:09 +0200

    move firebase config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseapp: {
+    apiKey: "AIzaSyD1PXQafOkXXeNXUMosgIJBTiXCJR2VcZ4",
+    authDomain: "auto-supers.firebaseapp.com",
+    databaseURL: "https://auto-supers.firebaseio.com",
+    projectId: "auto-supers",
+    storageBucket: "auto-supers.appspot.com",
+    messagingSenderId: "704428042217",
+    appId: "1:704428042217:web:fd51f7947499d45c9acd78",
+    measurementId: "G-E5V869E8MR"
+  }
 };
 
 /*
