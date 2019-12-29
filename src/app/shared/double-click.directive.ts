c35cbd3bdd33a28eb84b30f6c4107778810bf3e8
Ahmed Amr <ahmed.amr@businessboomers.net>
2019-12-29 17:10:56 +0200

    double click logic

--- src/app/shared/double-click.directive.ts
+++ src/app/shared/double-click.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, ElementRef } from '@angular/core';
+import { fromEvent, asyncScheduler } from 'rxjs';
+import { pluck, throttleTime } from 'rxjs/operators';
+import { BookmarksService } from '../bookmarks/bookmarks.service';
 
 @Directive({
   selector: '[appDoubleClick]'
 })
-export class DoubleClickDirective {
+export class DoubleClickDirective  implements OnInit{
 
-  constructor() { }
+  constructor(
+    private elemRef: ElementRef, 
+    private _bookmarksService: BookmarksService) { }
+
+  ngOnInit() {
+    fromEvent(this.elemRef.nativeElement, 'click')
+    .pipe(
+      pluck('target', 'parentElement', 'id'),
+      throttleTime(300, asyncScheduler, {trailing: true, leading: false})
+    )
+    .subscribe(id => {
+      this._bookmarksService.removeBookmarkById(+id);
+      this._bookmarksService.updateLocalStorage();
+    });
+  }
 
 }
