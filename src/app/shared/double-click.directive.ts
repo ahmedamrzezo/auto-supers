fdb297d11416fe20942dddbfd660e2e5d2939a3b
Ahmed Amr <ahmed.amr@businessboomers.net>
2019-12-31 10:18:59 +0200

    pass target as an anrgument

--- src/app/shared/double-click.directive.ts
+++ src/app/shared/double-click.directive.ts
@@ -1,7 +1,6 @@
-import { Directive, OnInit, ElementRef, Input, Output } from '@angular/core';
+import { Directive, OnInit, ElementRef, Output } from '@angular/core';
 import { fromEvent, asyncScheduler, Subject } from 'rxjs';
 import { pluck, throttleTime } from 'rxjs/operators';
-import { BookmarksService } from '../bookmarks/bookmarks.service';
 
 @Directive({
   selector: '[appDoubleClick]'
@@ -11,17 +10,16 @@ export class DoubleClickDirective  implements OnInit{
   @Output('doubleClickAction') actionSub = new Subject();
 
   constructor(
-    private elemRef: ElementRef, 
-    private _bookmarksService: BookmarksService) { }
+    private elemRef: ElementRef) { }
 
   ngOnInit() {
     fromEvent(this.elemRef.nativeElement, 'click')
     .pipe(
-      pluck('target', 'parentElement', 'id'),
+      pluck('target'),
       throttleTime(300, asyncScheduler, {trailing: true, leading: false})
     )
-    .subscribe(id => {
-      this.actionSub.next(id);
+    .subscribe(target => {
+      this.actionSub.next(target);
     });
   }
 
