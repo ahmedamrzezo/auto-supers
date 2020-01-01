3ef4192c5df5c62e77b18484f48483750706b40b
Ahmed Amr <ahmed.amr@businessboomers.net>
2020-01-01 22:49:50 +0200

    fix wrong target argument

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -30,8 +30,8 @@ export class BookmarksComponent implements OnInit, OnDestroy {
     .subscribe(val => (this.noBookmarks = val));
   }
 
-  deleteBookmark(target: HTMLElement) {
-    this._bookmarksService.removeBookmarkById(target.parentNode['id']);
+  deleteBookmark(id: number) {
+    this._bookmarksService.removeBookmarkById(id);
     this._bookmarksService.updateLocalStorage();
   }
 
