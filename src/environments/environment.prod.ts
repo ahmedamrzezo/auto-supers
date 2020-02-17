d158f1a6e4f76f76965184b3c540e4f6dc37303f
Ahmed Amr <ahmed.amr@businessboomers.net>
2020-02-17 22:37:40 +0200

    firebase initialize and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseapp: {
+    apiKey: "AIzaSyD1PXQafOkXXeNXUMosgIJBTiXCJR2VcZ4",
+    authDomain: "auto-supers.firebaseapp.com",
+    databaseURL: "https://auto-supers.firebaseio.com",
+    projectId: "auto-supers",
+    storageBucket: "auto-supers.appspot.com",
+    messagingSenderId: "704428042217",
+    appId: "1:704428042217:web:fd51f7947499d45c9acd78",
+    measurementId: "G-E5V869E8MR"
+  }
 };
