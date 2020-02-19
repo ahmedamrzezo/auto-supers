36b8cef79bba44cdc15b75bc16c0f6acb7a117cb
Ahmed Amr <ahmed.amr@businessboomers.net>
2020-02-19 22:51:30 +0200

    create guard and add logic

---
+++ src/app/collection/modify-form/modify-form.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SupercarEditComponent } from '../supercar-edit/supercar-edit.component';
+
+@Injectable(
+  {providedIn: 'root'}
+)
+export class modifyFormGuard implements CanDeactivate<SupercarEditComponent> {
+  canDeactivate(
+    component: SupercarEditComponent,
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    console.log(component);
+    if (component.superForm.touched && component.superForm.dirty) {
+      return confirm('Do you really want to cancel the modification?') ? true: false;
+    } else {
+      return true;
+    }
+  }
+}
