9637ac44d788d7cc27fba873495211d7a24693b7
Ahmed Amr <ahmed.amr@businessboomers.net>
2020-03-09 22:17:36 +0200

    remove consoles

--- src/app/shared/file-value-accessor.directive.ts
+++ src/app/shared/file-value-accessor.directive.ts
@@ -14,11 +14,10 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 export class FileValueAccessorDirective implements ControlValueAccessor{
 
   onChange(ev) {
-    console.log(ev);
   }
   onTouched(){}
   
   writeValue(value) {}
-  registerOnChange(fn: any) {this.onChange = fn; console.log(fn);}
-  registerOnTouched(fn: any) {this.onTouched = fn; console.log(fn);}
+  registerOnChange(fn: any) {this.onChange = fn;}
+  registerOnTouched(fn: any) {this.onTouched = fn;}
 }
