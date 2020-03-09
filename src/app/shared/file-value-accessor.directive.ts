14317334166df91c8db670c67a79d33dc5655206
Ahmed Amr <ahmed.amr@businessboomers.net>
2020-03-09 22:14:50 +0200

    create file accessor to let input accept any value

---
+++ src/app/shared/file-value-accessor.directive.ts
@@ -0,0 +1,24 @@
+import { Directive } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+@Directive({
+  selector: 'input[type="file"]',
+  host : {
+    '(change)' : 'onChange($event.target.files)',
+    '(blur)': 'onTouched()'
+  },
+  providers: [
+    {provide: NG_VALUE_ACCESSOR, useExisting: FileValueAccessorDirective, multi: true}
+  ]
+})
+export class FileValueAccessorDirective implements ControlValueAccessor{
+
+  onChange(ev) {
+    console.log(ev);
+  }
+  onTouched(){}
+  
+  writeValue(value) {}
+  registerOnChange(fn: any) {this.onChange = fn; console.log(fn);}
+  registerOnTouched(fn: any) {this.onTouched = fn; console.log(fn);}
+}
