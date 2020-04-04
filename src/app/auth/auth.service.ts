7446225b6a4730629411eed3eb0ff4eea9767494
AhmedAmr-bbn <ahmed.amr@businessboomers.net>
2020-04-04 21:42:13 +0200

    register and login methods

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FirebaseApp } from '@angular/fire';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _http: HttpClient, private fireAuth: FirebaseApp) { }
+
+  registerUser(email: string, password: string) {
+    return this.fireAuth.auth().createUserWithEmailAndPassword(email, password);
+  }
+
+  signInUser(email: string, password: string) {
+    return this.fireAuth.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  getCurrentUserToken() {
+    return this.fireAuth.auth().currentUser.getIdTokenResult();
+  }
 }
